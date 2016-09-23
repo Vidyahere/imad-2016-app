978105c8831b26badfb82cb30d78723235bd7440
Vidyalakshmi G <vidya.1996.lakshmi@gmail.com>
2016-09-23 13:13:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile('Article one will be served here');
+  res.send('Article one will be served here');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile('Article two will be served here');
+  res.send('Article two will be served here');
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile('Article three will be served here');
+  res.send('Article three will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
