d8b72cc8c838ee5c0145c068a9f6dbfdda7a42ad
Vidyalakshmi G <vidya.1996.lakshmi@gmail.com>
2016-09-23 19:22:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,21 @@
-
+var button= document.getElementById('counter');
+button.onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+     {
+        if(request.readyState === XMLHttpRequest.DONE) {
+           if(request.status === 200) {
+                  var counter = request.responseText;
+                  var span = document.getElementById('count');
+                   span.innerHTML= counter.toString();
+             }
+        
+          }
+     }
+     request.open("GET",'http://vidyahere.imad.hasura-app.io/',true);
+     request.send(null);
+};
+    
     
     
     
