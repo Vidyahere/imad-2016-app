64d6c10afb46d6ca78c1da62aad061cbf909036f
Vidyalakshmi G <vidya.1996.lakshmi@gmail.com>
2016-09-23 14:22:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'Article One | Vidya',
+  heading: 'Article One',
+  date: 'Sept 23, 2016',
+  content: `<p> This is content of Article One. This is content of Article One. This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.
+            </p>
+            <p> This is content of Article One. This is content of Article One. This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.
+            </p>
+            <p> This is content of Article One. This is content of Article One. This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.
+            </p>
+            <p> This is content of Article One. This is content of Article One. This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.
+            </p>
+            <p> This is content of Article One. This is content of Article One. This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.This is content of Article One.
+            </p> `
+};
+function createTemplate(data) {
+    var title= data.title;
+    var heading= data.heading;
+    var date= data.date;
+    var content = data.content;
+    var htmlTemplate = ` 
+<html>
+    <head>
+        <title> ${title} </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+        <style>
+        
+            
+        </style>
+    </head>
+    <body>
+        <div class="container">
+         <div>
+            <a href="/">Home</a>
+        </div>
+        <h3> ${heading} </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
